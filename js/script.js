79befc6d69811923dbf278b7518d8cd9b94acfdf
Maksim <maksimtrofimov13@gmail.com>
2021-04-18 18:26:32 +0300

    first lesson

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
-let number = 5;
-const leftBorderWidth = 1;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	jenres: [],
+	privat: false,
+};
+
+const firstMovie = prompt('Один из последних просмотренных фильмов?', '');
+const firstMovieScore = prompt('На сколько оцените его?', '');
+
+const secondMovie = prompt('Один из последних просмотренных фильмов?', '');
+const secondMovieScore = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[firstMovie] = firstMovieScore;
+personalMovieDB.movies[secondMovie] = secondMovieScore;
+
+console.log(personalMovieDB);
